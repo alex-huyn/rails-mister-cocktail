00b477fc1f4134646d251081400179313911fb77
Alex Huyn <huyn.alex@gmail.com>
2020-02-13 19:15:53 +0100

    Mister cocktail day1

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, :cocktail_id, :ingredient_id, presence: true
+  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
+end
